3531a0f9e99dec083d7e60bade8923270868849f
Balint Cristian <balint.cristan@gmail.com>
2017-03-18 00:56:47 +0100

    Chapter 6

--- money-example/src/main/java/Dollar.java
+++ money-example/src/main/java/Dollar.java
@@ -1,9 +1,7 @@
 /**
  * Created by Cristian on 2017.03.16..
  */
-public class Dollar {
-
-    private int amount;
+public class Dollar extends Money {
 
     Dollar(int amount) {
         this.amount = amount;
@@ -11,10 +9,5 @@ public class Dollar {
 
     Dollar times(int multiplier) {
         return new Dollar(amount * multiplier);
-    }
-
-    public boolean equals(Object object) {
-        Dollar dollar = (Dollar) object;
-        return amount == dollar.amount;
     }
 }
---
+++ money-example/src/main/java/Money.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Cristian on 2017.03.18..
+ */
+public class Money {
+
+    protected int amount;
+
+    public boolean equals(Object object) {
+        Money money = (Money) object;
+        return amount == money.amount;
+    }
+}
