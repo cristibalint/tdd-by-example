86339031a15fd5c4752f79018cfb07a40a7918fc
Balint Cristian <balint.cristan@gmail.com>
2017-03-18 11:52:19 +0100

    Chapter 11

--- money-example/src/main/java/Dollar.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Cristian on 2017.03.16..
- */
-public class Dollar extends Money {
-
-    Dollar(int amount, String currency) {
-        super(amount, currency);
-    }
-}
--- money-example/src/main/java/Franc.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Cristian on 2017.03.18..
- */
-public class Franc extends Money {
-
-    Franc(int amount, String currency) {
-        super(amount, currency);
-    }
-}
